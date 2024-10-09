7b4aba07a54f59dd77cc9069a04a95adcac5856c
aryasuat <aryasuat@hotmail.com>
2024-10-09 11:30:07 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            ViewBag.Active = 1;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "Hakkımda";
+            ViewBag.Active = 2;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "İletişim";
+            ViewBag.Active = 6;
+
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            ViewBag.Title = "Portfolyo";
+            ViewBag.Active = 5;
+
+            return View();
+        }
+
+        //public ActionResult PortfolioDetais()
+        //{
+        //    ViewBag.Title = "Portfolyo Detay";
+        //    ViewBag.Active = 5;
+
+        //    return View();
+        //}
+
+        public ActionResult Resume()
+        {
+            ViewBag.Title = "Öz Geçmiş";
+            ViewBag.Active = 3;
+
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            ViewBag.Title = "Home Page";
+            ViewBag.Active = 4;
+
+            return View();
+        }
+
+        public ActionResult StarterPage()
+        {
+            ViewBag.Title = "Home Page";
+            ViewBag.Active = 1;
+
+            return View();
+        }
+    }
+}
---
+++ Models/AdminContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Models
+{
+    public class AdminContext
+    {
+        //public AdminContext() : base("DefaultConnection")
+        //{
+
+        //}
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Content> Contents { get; set; }
+
+    }
+}
